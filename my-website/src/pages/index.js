1a4e8da24b800526a2799470e0c4f74d7f2e3f0e
kpdoddi <kpdoddi@gmail.com>
2024-08-19 10:48:34 -0700

    Update index.js

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -19,7 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <a
             className="button button--secondary button--lg"
-            href="https://github.com/couchbaselabs/CapellaDataStudioBuilds/releases/latest/download/Capella.DataStudio.zip"
+            href="https://github.com/couchbaselabs/CapellaDataStudioBuilds/releases/latest/download/Capella-DataStudio-darwin-x64.zip
+"
             target="_blank"
             rel="noopener noreferrer"
           >
